715134db975cfe3827063141271ec75fad3a0f98
Saicharan-05 <anupuramsaicharan18@gmail.com>
2026-06-29 17:25:44 +0530

    Create trsffic light module

---
+++ design.v
@@ -0,0 +1,11 @@
+module traffic_light_controller (
+  input clk,input rst,
+  output reg NS_R,
+  output reg NS_Y'
+  output reg NS_G,
+  output reg EW_R,
+  output reg EW_Y,
+  output reg EW_G
+);
+  
+endmodule
